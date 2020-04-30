1c55b7572b1e6884c646fc3ec118c900fb2255f2
wassimbel <wassim.belhedi1@gmail.com>
2020-04-30 15:01:09 -0400

    spaces give sense

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,5 +7,6 @@ elif len(sys.argv) == 2:
     print("1 argument:")
 else:
     print("{:d} arguments:".format(len(sys.argv)-1))
-    for i in range(1, len(sys.argv)):
-        print("{:d}: {:s}".format(i, sys.argv[i]))
+
+for i in range(1, len(sys.argv)):
+    print("{:d}: {:s}".format(i, sys.argv[i]))
