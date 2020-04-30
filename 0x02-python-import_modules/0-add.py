ceff195cff14fb52ba167dc8f911f5021ef2be6e
wassimbel <wassim.belhedi1@gmail.com>
2020-04-30 13:06:52 -0400

    import function add

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from add_0 import add
+
+
+a = 1
+b = 2
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+
+
+if __name__ == "__main__":
+    import sys
+    0-add(int(sys.argv[1]))
