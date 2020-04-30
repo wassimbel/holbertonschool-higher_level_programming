feaf86dde9d330a65c6aab066a08631b8070b218
wassimbel <wassim.belhedi1@gmail.com>
2020-04-30 14:54:02 -0400

    prints the number of and the list of its args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,6 @@ if len(sys.argv) == 1:
 elif len(sys.argv) == 2:
     print("1 argument:")
 else:
-    print("{:d} arguments:".format(len(sys.argv)))
+    print("{:d} arguments:".format(len(sys.argv)-1))
     for i in range(1, len(sys.argv)):
         print("{:d}: {:s}".format(i, sys.argv[i]))
