a1ea9f73633efe970913ca87bddd9c13df34db6b
wassimbel <wassim.belhedi1@gmail.com>
2020-05-05 17:55:41 -0400

    fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,4 +4,4 @@ def no_c(my_string):
     for i in range(len(my_string)):
         if my_string[i] not in 'Cc':
             new_string += my_string[i]
-            return new_string
+    return new_string
