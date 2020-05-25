aacf461a9fdb66a985810825ef786eaf63bf1295
wassimbel <wassim.belhedi1@gmail.com>
2020-05-25 18:01:17 -0400

    fix extra line

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -54,4 +54,4 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return str
         str += ("#" * self.__width + "\n") * self.__height
-        return str
+        return str[:-1]
