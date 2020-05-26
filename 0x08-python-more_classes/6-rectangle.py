beb1056b2df09658ca84bdf6e6daff9541506841
wassimbel <wassim.belhedi1@gmail.com>
2020-05-25 20:10:02 -0400

    update task

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -3,8 +3,8 @@
 
 
 class Rectangle:
-    number_of_instances = 0
     """class that defines rectangle"""
+    number_of_instances = 0
     def __init__(self, width=0, height=0):
         """initialize rectangle"""
         self.width = width
