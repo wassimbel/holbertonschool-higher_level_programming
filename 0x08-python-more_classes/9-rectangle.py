01136ca27d3234fd8ff92c1b5cf717f728dac469
wassimbel <wassim.belhedi1@gmail.com>
2020-05-25 21:06:15 -0400

    update task

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -79,7 +79,8 @@ class Rectangle:
         if rect_2.area() > rect_1.area():
             return rect_2
         return rect_1
+
     @classmethod
     def square(cls, size=0):
         """ defining class method square """
-        cls(size, size)
+        return cls(size, size)
