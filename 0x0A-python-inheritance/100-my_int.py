0e0899bb6112f6ed53b509dec1caace7defe386d
wassimbel <wassim.belhedi1@gmail.com>
2020-06-02 17:31:42 -0400

    update

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,10 +6,10 @@ module - Myint
 
 class MyInt(int):
     """ subclass MyInt """
-    def __eq__(self, inv):
-        """ inverse instance """
-        return self != inv
+     def __eq__(self, inv):
+        """eq"""
+        return not super().__eq__(inv)
 
-    def __ne__(self, other):
-        """ inverse too """
-        return self == inv
+    def __ne__(self, inv):
+        """not eq"""
+        return not super().__ne__(inv)
