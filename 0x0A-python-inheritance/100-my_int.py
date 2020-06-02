cf5751772ab2dbc54a8c07c0fe0f9d9f678e240a
wassimbel <wassim.belhedi1@gmail.com>
2020-06-02 17:25:25 -0400

    up

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -8,8 +8,8 @@ class MyInt(int):
     """ subclass MyInt """
     def __eq__(self, inv):
         """ inverse instance """
-        return int(self) != int(inv)
+        return self != inv
 
     def __ne__(self, other):
         """ inverse too """
-        return int(self) == int(inv)
+        return self == inv
