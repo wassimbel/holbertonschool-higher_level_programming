0689cb7b58ec7919e4ee69505d516156f8e54dac
wassimbel <wassim.belhedi1@gmail.com>
2020-06-02 17:46:12 -0400

    uodate

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,12 @@
+
+#!/usr/bin/python3
+""" module - function add_attribute """
+
+
+def add_attribute(obj, attr, val):
+    """ adds a new attribute to an object if it’s possible: """
+
+    if not hasattr(object, "__dict__"):
+        raise TypeError("can't add new attribute")
+
+    setattr(object, attribute, value)
