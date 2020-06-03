7f5b6cf844bf31f5309f78db5f00dc2a5042cadf
wassimbel <wassim.belhedi1@gmail.com>
2020-06-03 15:03:49 -0400

    fix

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -6,7 +6,7 @@ def read_lines(filename="", nb_lines=0):
     """  function that reads n lines of a text file (UTF8)
         and prints it to stdout  """
     with open(filename, encoding="utf-8") as myfile:
-        if nb_lines >= len(myfile.readlines()) or nb_lines <= 0:
+        if nb_lines <= 0:
             print(myfile.read(), end="")
         else:
             for j in range(nb_lines):
