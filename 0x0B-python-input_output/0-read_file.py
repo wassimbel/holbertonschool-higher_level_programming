6e1319d3d23b2121ffdba07f753a81c5299bc311
wassimbel <wassim.belhedi1@gmail.com>
2020-06-03 14:11:35 -0400

    try sthg else

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,5 @@
 def read_file(filename=""):
     """ function that reads a text file (UTF8) and prints it to stdout: """
     with open(filename, encoding="utf-8") as myfile:
-        print(myfile.read(), end="")
+        for i in myfile:
+            print(i, end="")
