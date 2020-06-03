b6e1b5837b7dd9a2356fe1e0aa09e41922c6d6d4
wassimbel <wassim.belhedi1@gmail.com>
2020-06-03 14:52:58 -0400

    fix

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -11,4 +11,4 @@ def read_lines(filename="", nb_lines=0):
                 print(i, end="")
             else:
                 for j in range(nb_lines):
-                    print(j, end="")
+                    print(i, end="")
