2e4c8de6763c2ff70bfa5802cbda72a8aad375ae
wassimbel <wassim.belhedi1@gmail.com>
2020-06-03 16:09:23 -0400

    creates an Object from a “JSON file”:

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -4,6 +4,6 @@ import json
 
 
 def load_from_json_file(filename):
-""" function that creates an Object from a “JSON file """
+    """ function that creates an Object from a “JSON file """
     with open(filename, mode="w") as myfile:
         return json.load(myfile)
