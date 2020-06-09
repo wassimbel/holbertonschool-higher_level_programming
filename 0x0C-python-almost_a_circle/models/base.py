dffa0a4a918bdb8500439430f1f73e1fb520625b
wassimbel <wassim.belhedi1@gmail.com>
2020-06-09 17:48:10 -0400

    fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -43,3 +43,17 @@ class Base:
         if json_string is None:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """ returns an instance with all attributes already set """
+
+        if cls.__name__ == 'Rectangle':
+            dummy = cls(1, 1)
+
+        else:
+            dummy = cls(1)
+
+        dummy.update(**dictionary)
+        return dummy
+
