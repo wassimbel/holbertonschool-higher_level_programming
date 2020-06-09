8e8e37eeba1c83ea358141c2ddc5108a1f5a998a
wassimbel <wassim.belhedi1@gmail.com>
2020-06-09 17:30:11 -0400

    fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,7 +4,7 @@ import json
 
 
 class Base:
-    """ This class will be the “base” of all other classes in this project.
+    """  This class will be the “base” of all other classes in this project.
         The goal of it is to manage id attribute in all your future classes
         and to avoid duplicating the same code (by extension, same bugs) """
     __nb_objects = 0
