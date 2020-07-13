ac004eb67ba793a28214ee9f5b964f0f0356c666
wassimbel <wassim.belhedi1@gmail.com>
2020-07-13 17:53:18 -0400

    ?

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
---  script that computes the score average of all records in the table
+-- script that computes the score average of all records in the table
 -- second_table of the database hbtn_0c_0
 SELECT AVG(score) AS average from second_table;
