c5bc4a5fca3d2f2c390f9a7e6aa5f55cf09f7dd3
wassimbel <wassim.belhedi1@gmai.com>
2020-07-14 16:11:52 -0400

    ??

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,3 @@
--- lists all privileges of the MySQL user_0d_1 and user_0d_1
+-- users user_0d_1 and user_0d_2 in localhost
 SHOW GRANTS FOR user_0d_1@localhost;
 SHOW GRANTS FOR user_0d_2@localhost;
-
-
