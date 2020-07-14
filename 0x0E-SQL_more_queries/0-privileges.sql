495786541ee08458bdb4f5bdbda999de6a88d3a3
wassimbel <wassim.belhedi1@gmai.com>
2020-07-14 16:15:11 -0400

    wn

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,6 @@
--- users user_0d_1 and user_0d_2 in localhost
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2
+-- from MySQL server
 SHOW GRANTS FOR user_0d_1@localhost;
 SHOW GRANTS FOR user_0d_2@localhost;
+
+
