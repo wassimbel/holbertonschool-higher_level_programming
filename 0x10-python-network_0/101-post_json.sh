d10c289516c7ce527c46516797fe302b44fa8892
wassimbel <wassim.belhedi1@gmail.com>
2020-08-18 22:28:13 +0000

    update

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# sends a JSON POST request to a URL passed as the first argument,
-# and displays the body of the response.
+# sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
 curl -sX POST -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
+
--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Write a Bash script that takes in a URL as an argument,
-# sends a GET request to the URL, and displays the body of the response
+# Write a Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
 curl -s -X GET -H "X-HolbertonSchool-User-Id: 98" "$1"
 
