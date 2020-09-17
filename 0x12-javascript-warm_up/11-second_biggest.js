684a70b2b75445439150af2dbdbe5777520c124e
wassimbel <wassim.belhedi1@gmail.com>
2020-09-17 15:05:06 -0400

    fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@ const len = process.argv.length - 2;
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  let a = []
+  let a = [];
   a = process.argv.slice(2).sort();
   console.log(a[len - 2]);
 }
