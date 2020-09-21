1b9dbd4ca1a1815151d22a0254841db35ecb4398
wassimbel <wassim.belhedi1@gmail.com>
2020-09-21 14:45:40 -0400

    X

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -11,7 +11,7 @@ module.exports = class Rectangle {
 
   print () {
     for (let i = 0; i < this.height; i++) {
-      console.log('#'.repeat(this.width));
+      console.log('X'.repeat(this.width));
     }
   }
 };
