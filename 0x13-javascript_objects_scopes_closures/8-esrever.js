e1b15aa71e8ca6d96c81a1dd724d7a284edf9a95
wassimbel <wassim.belhedi1@gmail.com>
2020-09-21 14:57:36 -0400

    semi fix

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
 //  return list.reverse();
-  let rev = [];
+  const rev = [];
   let i = 0;
   for (let j = list.length - 1; j >= 0; j--, i++) {
     rev[j] = list[i];
