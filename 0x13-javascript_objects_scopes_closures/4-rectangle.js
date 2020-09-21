35c5bf2385f9e1a7580e71df87c3a20b342660cc
wassimbel <wassim.belhedi1@gmail.com>
2020-09-21 14:41:45 -0400

    fix

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -2,7 +2,7 @@
 module.exports = class Rectangle {
   constructor (w, h) {
     if (w <= 0 || isNaN(w) || h <= 0 || isNaN(h)) {
-
+      return;
     } else {
       this.width = w;
       this.height = h;
