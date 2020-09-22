ef786c4d5b1cbb1afa5ea59000cc156c289aba7b
wassimbel <wassim.belhedi1@gmail.com>
2020-09-22 12:28:38 -0400

    return

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', function (error, data) {
   if (error) {
-    console.log(error);
+    return console.log(error);
   }
   console.log(data);
 });
